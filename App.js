66f2df19a3406fa3929fc2140175ba36edf6be3b
cold_hailstone <cksn1993@gmail.com>
2023-11-17 13:34:04 +0900

    2.6 Styles part Two

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StatusBar, StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function App() {
     return (
@@ -7,17 +7,35 @@ export default function App() {
             <View style={styles.city}>
                 <Text style={styles.cityName}>Seoul</Text>
             </View>
-            <View style={styles.weather}>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.weather}
+            >
                 <View style={styles.day}>
                     <Text style={styles.temp}>27</Text>
                     <Text style={styles.description}>Sunny</Text>
                 </View>
-            </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.description}>Sunny</Text>
+                </View>
+            </ScrollView>
             <StatusBar barStyle='default' />
         </View>
     );
 }
 
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -32,11 +50,9 @@ const styles = StyleSheet.create({
         fontSize: 68,
         fontWeight: '500',
     },
-    weather: {
-        flex: 3,
-    },
+    weather: {},
     day: {
-        flex: 1,
+        width: SCREEN_WIDTH,
         alignItems: 'center',
     },
     temp: {
